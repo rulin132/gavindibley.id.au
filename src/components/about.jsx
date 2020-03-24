7249767708e4d1747b9b6ebe267f966212944369
Gavin Dibley <gavin@advisercentre.com.au>
2020-03-24 12:53:51 +1100

    Add link to my Github account

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 export default class About extends Component {
   render() {
     return (
@@ -15,6 +16,11 @@ export default class About extends Component {
                             I have over a decade of professional experience in website development and have worked with a variety of clients big and small 
                             this includes clients in the government, education industry, financial sector and large multi-national organisations, there is no job too big or small.</p>
 
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-lg-8 text-center">
+                        <a href="https://github.com/rulin132"><FontAwesomeIcon icon={faGithub} className=" fa-4x mb-3 text-white" /></a>
                     </div>
                 </div>
             </div>
