a8c9af303abbdfbeea38c41757d834cc653b6f9d
Gavin Dibley <gavin@advisercentre.com.au>
2020-03-26 09:49:00 +1100

    Update About text to correct grammar and add family line

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,13 +10,13 @@ export default class About extends Component {
                     <div className="col-lg-8 text-center">
                         <h2 className="text-white mt-0">About Me</h2>
                         <hr className="divider light my-4" />
-                        <p className="text-white-50 mb-4">My name is Gavin Dibley and I'm a professional website designer and developer located on the Central Coast.</p>
+<p className="text-white-50 mb-4">My name is Gavin Dibley and I'm a professional website designer and developer located on the Central Coast. I'm also a devoted father of two children and a loving partner.</p>
 
-                        <p className="text-white-50 mb-4">
-                            I have over a decade of professional experience in website development and have worked with a variety of clients big and small 
-                            this includes clients in the government, education industry, financial sector and large multi-national organisations, there is no job too big or small.</p>
+<p className="text-white-50 mb-4">
+    I have over a decade of professional experience in website development and have worked with a variety of clients big and small 
+    which includes clients in the government, education industry, financial sector and large multi-national organisations. There is no jobs too big or small.</p>
 
-                    </div>
+</div>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-lg-8 text-center">
