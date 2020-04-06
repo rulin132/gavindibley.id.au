086127f5e7ef852b359de329456cec2210138682
Gavin Dibley <gavin@advisercentre.com.au>
2020-04-07 00:47:18 +1000

    Change code for minor improvement of readability

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ export default class Navbar extends Component {
         <div className="container h-100">
         <div className="row h-100 align-items-center justify-content-center text-center">
             <div className="col-lg-10 align-self-end">
-            <h1 className="text-uppercase font-weight-bold"><img src="/images/logo.png" /></h1>
+            <h1 className="text-uppercase font-weight-bold"><img src="/images/logo.png" alt="Gavin Dibley" /></h1>
             <hr className="divider my-4" />
             </div>
             <div className="col-lg-8 align-self-baseline">
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,43 +1,34 @@
 import React, { Component } from 'react'
-import {
-    Route,
-    NavLink
-  } from "react-router-dom";
-
-  import { HashLink as Link, NavHashLink } from 'react-router-hash-link';
-
-
-  import Scrollspy from "react-scrollspy";
+import { HashLink as Link } from 'react-router-hash-link';
+import Scrollspy from "react-scrollspy";
 
 export default class Navbar extends Component {
+  state = {
+    isTop: true,
+  };
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
 
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== this.state.isTop) {
+          this.setState({ isTop })
+      }
+    });
+  }
 
-    state = {
-        isTop: true,
-      };
-      constructor(props) {
-        super(props);
-        this.myRef = React.createRef();
-      }
-      
-      componentDidMount() {
-        document.addEventListener('scroll', () => {
-          const isTop = window.scrollY < 100;
-          if (isTop !== this.state.isTop) {
-              this.setState({ isTop })
-          }
-        });
-      }
-
-      isActive() {
-        return 'navbar navbar-expand-lg navbar-light fixed-top py-3 btn '+ (!this.state.isTop ? 'navbar-scrolled' : '');
-      }
+  isActive() {
+    return 'navbar navbar-expand-lg navbar-light fixed-top py-3 btn '+ (!this.state.isTop ? 'navbar-scrolled' : '');
+  }
 
   render() {
     return (
         <nav className={this.isActive()} id="mainNav">
             <div className="container">
-                <Link exact="true" to="/" className="navbar-brand"><img src="/images/logo.webp" style={{height: "26px"}} /></Link>
+                <Link exact="true" to="/" className="navbar-brand"><img src="/images/logo.webp" style={{height: "26px"}} alt="Gavin Dibley" /></Link>
         
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
