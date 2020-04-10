67a4441fbf20acd6e4674faf8d6908d03006f5e2
Gavin Dibley <gavin@advisercentre.com.au>
2020-04-10 22:06:43 +1000

    Update App.test.js to ensure it passes rendering content

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders My name is Gavin Dibley', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/My name is Gavin Dibley/i);
   expect(linkElement).toBeInTheDocument();
 });
